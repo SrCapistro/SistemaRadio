648d63c9fb6a94f8b2545be5797c7c3a9b9c807b
luzita_ivanita@hotmail.com <luzita_ivanita@hotmail.com>
2021-11-22 14:25:20 -0600

    Agregué la clase de Artista en POCO

---
+++ Implementación/SistemaDeRadio/SistemaDeRadio/POCO/Artista.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaDeRadio.POCO
+{
+    class Artista
+    {
+        private long artistaID;
+        private String artistaNombre;
+        private String artistaEstado;
+
+        public long ArtistaID { get => artistaID; set => artistaID = value; }
+        public string ArtistaNombre { get => artistaNombre; set => artistaNombre = value; }
+        public string ArtistaEstado { get => artistaEstado; set => artistaEstado = value; }
+    }
+}
